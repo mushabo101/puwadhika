6b8dcd19aa59ed4b40edca56606d2b2b2d4460a9
mushabo101 <72403568+mushabo101@users.noreply.github.com>
2021-03-16 21:27:37 +0700

    Delete Day 10 Challenge Bank Soal 3 No 1.py

--- Day 10 Challenge Bank Soal 3 No 1.py
+++
@@ -1,9 +0,0 @@
-a = input('Masukan Sebuah Kalimat : ')
-
-if len(a) != 0 and len(a) < 200:
-    print('*'+a.upper()+'*')
-elif len(a) > 200:
-    print('Batas Karakter Maksimal Hanya 200 ')
-else :
-    b = input('Masukkan Sebuah Inputan')
-    
